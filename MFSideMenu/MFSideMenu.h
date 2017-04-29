7fa591ef82ba28ea325fc092bcce1a894799e4b9
Hieu Phan Q <hieupq@dwarvesf.com>
2017-04-29 12:57:42 +0700

    Push latest data

---
+++ MFSideMenu/MFSideMenu.h
@@ -0,0 +1,20 @@
+//
+//  MFSideMenu.h
+//  MFSideMenu
+//
+//  Created by Hieu Phan on 4/17/17.
+//  Copyright © 2017 Delivr Company Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MFSideMenu.
+FOUNDATION_EXPORT double MFSideMenuVersionNumber;
+
+//! Project version string for MFSideMenu.
+FOUNDATION_EXPORT const unsigned char MFSideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFSideMenu/PublicHeader.h>
+
+#import "MFSideMenuContainerViewController.h"
+#import "UIViewController+MFSideMenuAdditions.h"
